9de90232b62215c41347c14715e2e3f598289d69
GerardGZ <gerardgimenez1014@gmail.com>
2026-01-01 16:01:56 -0500

    model is good

--- models/model.py
+++ models/model.py
@@ -1,58 +1,95 @@
+# model.py
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class UNet(nn.Module):
-    def __init__(self, in_channels=3, num_classes=3, features=[64, 128, 256, 512]):
-        super(UNet, self).__init__()
-        self.downs = nn.ModuleList()
-        self.ups = nn.ModuleList()
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        # Downsampling path
-        for feature in features:
-            self.downs.append(self.double_conv(in_channels, feature))
-            in_channels = feature
-
-        # Upsampling path
-        for feature in reversed(features):
-            self.ups.append(nn.ConvTranspose2d(feature*2, feature, kernel_size=2, stride=2))
-            self.ups.append(self.double_conv(feature*2, feature))
-
-        # Final conv layer
-        self.final_conv = nn.Conv2d(features[0], num_classes, kernel_size=1)
+class DoubleConv(nn.Module):
+    """
+    Two consecutive convolution layers with BatchNorm + ReLU
+    """
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
 
     def forward(self, x):
-        skip_connections = []
+        return self.conv(x)
+
+
+class UNet(nn.Module):
+    """
+    UNet for binary segmentation (1 channel output)
+    """
+    def __init__(self, in_channels=3, out_channels=1):
+        super(UNet, self).__init__()
 
         # Downsampling
-        for down in self.downs:
-            x = down(x)
-            skip_connections.append(x)
-            x = self.pool(x)
-
-        skip_connections = skip_connections[::-1]  # reverse for upsampling
+        self.dconv_down1 = DoubleConv(in_channels, 64)
+        self.dconv_down2 = DoubleConv(64, 128)
+        self.dconv_down3 = DoubleConv(128, 256)
+        self.dconv_down4 = DoubleConv(256, 512)
+        self.maxpool = nn.MaxPool2d(2)
 
         # Upsampling
-        for idx in range(0, len(self.ups), 2):
-            x = self.ups[idx](x)  # transpose conv
-            skip_connection = skip_connections[idx//2]
+        self.upsample4 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.dconv_up3 = DoubleConv(512, 256)
 
-            # Resize if necessary (due to rounding)
-            if x.shape != skip_connection.shape:
-                x = F.interpolate(x, size=skip_connection.shape[2:])
+        self.upsample3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.dconv_up2 = DoubleConv(256, 128)
 
-            x = torch.cat((skip_connection, x), dim=1)
-            x = self.ups[idx+1](x)  # double conv
+        self.upsample2 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.dconv_up1 = DoubleConv(128, 64)
 
-        return self.final_conv(x)
+        # Final output
+        self.conv_last = nn.Conv2d(64, out_channels, kernel_size=1)
 
-    def double_conv(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-        )
+    def forward(self, x):
+        # Downsampling
+        conv1 = self.dconv_down1(x)
+        conv2 = self.dconv_down2(self.maxpool(conv1))
+        conv3 = self.dconv_down3(self.maxpool(conv2))
+        conv4 = self.dconv_down4(self.maxpool(conv3))
+
+        # Upsampling with size fix
+        x = self.upsample4(conv4)
+        x = F.interpolate(x, size=(conv3.size(2), conv3.size(3)), mode='bilinear', align_corners=False)
+        x = torch.cat([x, conv3], dim=1)
+        x = self.dconv_up3(x)
+
+        x = self.upsample3(x)
+        x = F.interpolate(x, size=(conv2.size(2), conv2.size(3)), mode='bilinear', align_corners=False)
+        x = torch.cat([x, conv2], dim=1)
+        x = self.dconv_up2(x)
+
+        x = self.upsample2(x)
+        x = F.interpolate(x, size=(conv1.size(2), conv1.size(3)), mode='bilinear', align_corners=False)
+        x = torch.cat([x, conv1], dim=1)
+        x = self.dconv_up1(x)
+
+        out = self.conv_last(x)
+        return out
+
+
+def get_model(in_channels=3, out_channels=1, device=None):
+    """
+    Instantiate UNet and move to device
+    """
+    model = UNet(in_channels=in_channels, out_channels=out_channels)
+    if device is None:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+    return model.to(device)
+
+
+# Quick test
+if __name__ == "__main__":
+    model = get_model()
+    print(model)
+    x = torch.randn(1, 3, 512, 512)
+    y = model(x)
+    print(f"Output shape: {y.shape}")
